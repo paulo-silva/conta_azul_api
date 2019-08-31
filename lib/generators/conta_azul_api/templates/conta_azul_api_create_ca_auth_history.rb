3201d57f47bc35a0b46d99cdccd388c407559b5d
Paulo Henrique da Silva <paulo.silva1337@gmail.com>
2019-08-31 19:08:51 -0300

    Rename migration.

--- lib/generators/conta_azul_api/templates/conta_azul_api_create_ca_auth_history.rb
+++ lib/generators/conta_azul_api/templates/conta_azul_api_create_ca_auth_history.rb
@@ -1,6 +1,6 @@
-class ContaAzulApiCreateCaAuthHistory < ActiveRecord::Migration[5.2]
+class ContaAzulApiCreateCaAuthHistories < ActiveRecord::Migration[5.2]
   def change
-    create_table :ca_auth_history do |t|
+    create_table :ca_auth_histories do |t|
       t.string   :access_token,  null: false
       t.string   :refresh_token, null: false
       t.datetime :expires_at,    null: false
