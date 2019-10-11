bfbda67a91cbf0d49b05fbab077fbc06502a9b8e
Matheus Cendrão <matheuscendrao@gmail.com>
2019-10-11 16:58:45 -0300

    Apply PR suggestion to change tests from DRY to DUMP

--- spec/conta_azul_api/http_response_spec.rb
+++ spec/conta_azul_api/http_response_spec.rb
@@ -2,33 +2,34 @@ RSpec.describe ContaAzulApi::HttpResponse do
   let(:product_payload) { File.read('spec/fixtures/products_endpoints/find_by_id.json') }
 
   before do
-    stub_request(:get, 'https://api.contaazul.com/v1/products/c7288c09-829d-48b9-aee2-4f744e380587').
-      to_return(status: response_status, body: product_payload, headers: {})
-
     logger = double(:logger, info: '')
     allow(Rails).to receive(:logger).and_return(logger)
   end
 
-  subject(:response) do
-    ContaAzulApi::Request.new.get(
-      endpoint: 'v1/products/c7288c09-829d-48b9-aee2-4f744e380587',
-      authorization: 'fake_auth'
-    )
-  end
-
   describe '#status_code' do
     context 'when it is a successfull request' do
-      let(:response_status) { 200 }
-
       it 'returns the response status code' do
+        stub_request(:get, 'https://api.contaazul.com/v1/products/c7288c09-829d-48b9-aee2-4f744e380587').
+          to_return(status: 200, body: product_payload, headers: {})
+
+        response = ContaAzulApi::Request.new.get(
+          endpoint: 'v1/products/c7288c09-829d-48b9-aee2-4f744e380587',
+          authorization: 'fake_auth'
+        )
         expect(response.status_code).to eq(:ok)
       end
     end
 
     context 'when it is a unsuccessfull request' do
-      let(:response_status) { 404 }
-
       it 'returns the response status code' do
+        stub_request(:get, 'https://api.contaazul.com/v1/products/c7288c09-829d-48b9-aee2-4f744e380587').
+          to_return(status: 404, body: product_payload, headers: {})
+
+        response = ContaAzulApi::Request.new.get(
+          endpoint: 'v1/products/c7288c09-829d-48b9-aee2-4f744e380587',
+          authorization: 'fake_auth'
+        )
+
         expect(response.status_code).to eq(:not_found)
       end
     end
@@ -36,26 +37,44 @@ RSpec.describe ContaAzulApi::HttpResponse do
 
   describe '#success?' do
     context 'when it is a successfull request' do
-      let(:response_status) { 200 }
-
       it 'returns true when success' do
+        stub_request(:get, 'https://api.contaazul.com/v1/products/c7288c09-829d-48b9-aee2-4f744e380587').
+          to_return(status: 200, body: product_payload, headers: {})
+
+        response = ContaAzulApi::Request.new.get(
+          endpoint: 'v1/products/c7288c09-829d-48b9-aee2-4f744e380587',
+          authorization: 'fake_auth'
+        )
+
         expect(response.success?).to be true
       end
     end
 
     context 'when it is a unsuccessfull request' do
-      let(:response_status) { 404 }
-
       it 'returns true when success' do
+        stub_request(:get, 'https://api.contaazul.com/v1/products/c7288c09-829d-48b9-aee2-4f744e380587').
+          to_return(status: 404, body: product_payload, headers: {})
+
+        response = ContaAzulApi::Request.new.get(
+          endpoint: 'v1/products/c7288c09-829d-48b9-aee2-4f744e380587',
+          authorization: 'fake_auth'
+        )
+
         expect(response.success?).to be false
       end
     end
   end
 
   describe '#body' do
-    let(:response_status) { 200 }
-
     it 'returns a hash format body' do
+      stub_request(:get, 'https://api.contaazul.com/v1/products/c7288c09-829d-48b9-aee2-4f744e380587').
+        to_return(status: 200, body: product_payload, headers: {})
+
+      response = ContaAzulApi::Request.new.get(
+        endpoint: 'v1/products/c7288c09-829d-48b9-aee2-4f744e380587',
+        authorization: 'fake_auth'
+      )
+
       expect(response.body).to be_a Hash
       expect(response.body).to eq(JSON.parse(product_payload))
     end
