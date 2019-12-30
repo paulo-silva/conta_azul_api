edd8bf8a493103db2e23c641eea75468b0a76d2d
Paulo Henrique da Silva <paulo.silva@plataformatec.com.br>
2019-12-30 08:38:51 -0300

    Expose refresh oauth token stub.

--- lib/conta_azul_api/helpers.rb
+++ lib/conta_azul_api/helpers.rb
@@ -1,8 +1,15 @@
 module ContaAzulApi
   module Helpers
     def self.stub_refresh_token
+      body = {
+        "refresh_token": "YYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY",
+        "token_type": "bearer",
+        "access_token": "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+        "expires_in": 3600
+      }.to_json
+
       WebMock.stub_request(:post, %r{https://api.contaazul.com/oauth2/token}).
-        to_return(status: 200, body: File.read('spec/fixtures/refresh_oauth_token.json'), headers: {})
+        to_return(status: 200, body: body, headers: {})
     end
   end
 end
